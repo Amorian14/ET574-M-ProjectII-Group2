748ab55f5b12b1be714d42b8360388cd020701be
Kenn J Scott <52748543+vexykenn@users.noreply.github.com>
2026-04-24 23:29:59 -0400

    Added circumference_of_circle function

--- my_math.py
+++ my_math.py
@@ -6,7 +6,7 @@ def abs_val(x):
     Returns: positive value of x
     """
 
-    if not isinstance(x, (int, float)):
+    if isinstance(x, bool) or not isinstance(x, (int, float)):
         raise TypeError("abs_val(x) requires a number")
 
     if x < 0:
@@ -22,10 +22,26 @@ def area_of_circle(r):
     Returns: area of the circle
     """
 
-    if not isinstance(r, (int, float)):
+    if isinstance(r, bool) or not isinstance(r, (int, float)):
         raise TypeError("area_of_circle(r) requires a number")
 
     if r < 0:
         raise ValueError("radius cannot be negative")
 
     return 3.1416 * r * r
+
+def circumference_of_circle(r):
+    """
+    Name: circumference_of_circle
+    Purpose: Calculates the circumference of a circle
+    Parameters: r (radius)
+    Returns: circumference of the circle
+    """
+
+    if isinstance(r, bool) or not isinstance(r, (int, float)):
+        raise TypeError("circumference_of_circle(r) requires a number")
+
+    if r < 0:
+        raise ValueError("radius cannot be negative")
+
+    return 2 * 3.1416 * r
